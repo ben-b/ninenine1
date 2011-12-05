08b05ec45b9d2efbaeb251be237f4320c3037f07
ben_b <ben@freegeekchicago.org>
2011-12-05 16:32:48 -0500

    why the hell do the forwarding numbers have quotes?

--- events.rb
+++ events.rb
@@ -9,14 +9,35 @@ GoogleText.configure do |c|
     c.password     = config["credentials"]["password"]
 end
 
+client  = GoogleText::Client.new
+
+if !client.logged_in? then
+        client.login
+end
+
+page            = client.session.get("https://www.google.com/voice#phones")
+
+forwarderDataBlock      = /^\s*'phones':.*,$/.match(page)
+
+forwardingNumbers       = forwarderDataBlock.to_s.scan(/(\+\d{11,11})/)
+
+p forwardingNumbers[1][0]
+
 EventMachine.run {
   EventMachine.add_periodic_timer(10) {
     messages = GoogleText::Message.unread
       if messages
 	#puts GoogleText::Message.unread.inspect
         messages.each { |message|
-          display_number = message.display_number.scan(/\d*/).join
-          puts "New message from \+1"+display_number
+          display_number = "+1"+message.display_number.scan(/\d*/).join
+          puts display_number
+          puts "New message from "+display_number
+          if forwardingNumbers.include?(display_number)
+            p "This message is from a dispatcher."
+          else
+            p "This message is not from a dispatcher."           
+          end
+
         }
       else
         puts "no new messages"
