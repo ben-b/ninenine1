d19a040fbc0f88b5133ecc18429db4cc0fce9c14
ben_b <ben@freegeekchicago.org>
2011-12-15 21:14:10 -0500

    all logic is complete and in place.  attempting to send the dispatch currently returns an unhandled 403 error.

--- events.rb
+++ events.rb
@@ -90,22 +90,40 @@ module Dispatch
     client.forwarding_numbers.include?(sender)
   end
 
+  def first_responders(groups=client.forwarding_numbers)
+
+    fr_a = []
+
+    groups.each { |group_name|
+     client.contacts[client.groups[group_name]].each { |number|
+
+        if !fr_a 
+          fr_a = [number]
+        elsif !fr_a.include?(number)
+          fr_a.push(number)
+        end
+
+      }
+    }
+    first_responders = fr_a
+
+  end
+
   def dispatch
 
     body = text.match(/(.*)!!(.*)/)
     if !body
       puts "Message is not a properly formatted dispatch.  It must start with a list of recipient group codes terminated by '!!'"
     else
-      groups = body[1]
+      groups = body[1].split(" ")
       body = body[2]
     end
 
-    #this needs google contacts magic
-    # recip_list = transform_list_of_group_codes_to_list_of_phone_numbers(groups)
-    #recip_list.each { |recip|
-    #  dispatch = GoogleText::message.new(:text => body, :to => recip)
-    #  dispatch.send
-    #}
+    first_responders(groups).each { |fr|
+      dispatch = GoogleText::Message.new(:text => body, to => fr)
+      dispatch.send
+    }
+
   end
 
 end
@@ -118,9 +136,8 @@ class GoogleText::Message
   include Dispatch
 end
 
-=begin
 #EventMachine.run {
-#  EventMachine.add_periodic_timer(10) {
+# EventMachine.add_periodic_timer(10) {
     messages = GoogleText::Message.unread
 
     if messages 
@@ -136,4 +153,3 @@ end
     end
 #  }
 #}
-=end
